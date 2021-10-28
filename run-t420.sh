f3c6f540a46e456c790732b744b1eb12dfd834fa
Reto Schneider <reto.schneider@husqvarnagroup.com>
2021-10-28 14:31:19 +0200

    fixup! Helper script to run/execute rtl8xxxu on my T420

--- run-t420.sh
+++ run-t420.sh
@@ -2,7 +2,6 @@
 
 set -eu -o pipefail
 
-scp drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu.ko t420:rtl8xxxu-qt-creator.ko
+scp drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu.ko t420:rtl8xxxu.ko
 ssh t420 "sudo rmmod rtl8xxxu rtl8192cu rtl8192c_common rtl_usb rtlwifi 8192c 2>&- | true"
-ssh t420 "echo -n 'module rtl8xxxu +p' | sudo tee /sys/kernel/debug/dynamic_debug/control >/dev/null"
-ssh t420 "sudo insmod rtl8xxxu-qt-creator.ko debug=0xF8000; echo -n 'module rtl8xxxu +p' | sudo tee /sys/kernel/debug/dynamic_debug/control >/dev/null"
+ssh t420 "sudo insmod rtl8xxxu.ko debug=0xF8000; echo -n 'module rtl8xxxu +p' | sudo tee /sys/kernel/debug/dynamic_debug/control >/dev/null"
