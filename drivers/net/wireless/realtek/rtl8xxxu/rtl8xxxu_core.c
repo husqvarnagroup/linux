93033e68d891a97748876085d6a16614bdb3a1bc
Reto Schneider <reto.schneider@husqvarnagroup.com>
2021-11-12 11:10:05 +0100

    rtl8xxxu: 8188cus: Adjust retry values to 8192cu

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
@@ -1732,9 +1732,9 @@ rtl8xxxu_set_retry(struct rtl8xxxu_priv *priv, u16 short_retry, u16 long_retry)
 {
 	u16 val16;
 
-	val16 = ((short_retry << RETRY_LIMIT_SHORT_SHIFT) &
+	val16 = ((0x30 << RETRY_LIMIT_SHORT_SHIFT) &
 		 RETRY_LIMIT_SHORT_MASK) |
-		((long_retry << RETRY_LIMIT_LONG_SHIFT) &
+		((0x30 << RETRY_LIMIT_LONG_SHIFT) &
 		 RETRY_LIMIT_LONG_MASK);
 
 	rtl8xxxu_write16(priv, REG_RETRY_LIMIT, val16);
