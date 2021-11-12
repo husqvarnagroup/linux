170f2c3e2a30d64d4b48e268221812c7d09ea779
Reto Schneider <reto.schneider@husqvarnagroup.com>
2021-11-12 05:07:08 +0100

    rtl8xxxu: 8188cus: Adjust REG_AMPDU_MIN_SPACE to 8192cu

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
@@ -3798,12 +3798,7 @@ rtl8xxxu_set_ampdu_factor(struct rtl8xxxu_priv *priv, u8 ampdu_factor)
 
 static void rtl8xxxu_set_ampdu_min_space(struct rtl8xxxu_priv *priv, u8 density)
 {
-	u8 val8;
-
-	val8 = rtl8xxxu_read8(priv, REG_AMPDU_MIN_SPACE);
-	val8 &= 0xf8;
-	val8 |= density;
-	rtl8xxxu_write8(priv, REG_AMPDU_MIN_SPACE, val8);
+	rtl8xxxu_write8(priv, REG_AMPDU_MIN_SPACE, 0x07);
 }
 
 static int rtl8xxxu_active_to_emu(struct rtl8xxxu_priv *priv)
