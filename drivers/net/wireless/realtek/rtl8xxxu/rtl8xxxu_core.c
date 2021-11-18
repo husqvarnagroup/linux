735bdc629633b15f48a0acdaee3802a47a08fd1d
Reto Schneider <reto.schneider@husqvarnagroup.com>
2021-11-18 16:26:21 +0100

    rtl8xxxu: update_rate_mask: Print SGI flag

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
@@ -4594,8 +4594,9 @@ void rtl8xxxu_update_rate_mask(struct rtl8xxxu_priv *priv,
 	if (sgi)
 		h2c.ramask.arg |= 0x20;
 
-	dev_dbg(&priv->udev->dev, "%s: rate mask %08x, arg %02x, size %zi\n",
-		__func__, ramask, h2c.ramask.arg, sizeof(h2c.ramask));
+	dev_dbg(&priv->udev->dev,
+		"%s: rate mask %08x, arg %02x, size %zi, sgi %d\n", __func__,
+		ramask, h2c.ramask.arg, sizeof(h2c.ramask),sgi);
 	rtl8xxxu_gen1_h2c_cmd(priv, &h2c, sizeof(h2c.ramask));
 }
 
